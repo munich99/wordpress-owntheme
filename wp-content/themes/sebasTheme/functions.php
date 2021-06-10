97e65c1286198fa6f19f0509a18962f5b51c3d18
munich99 <regerstrasse@icloud.com>
2021-06-10 10:13:19 +0200

    add menue

--- wp-content/themes/sebasTheme/functions.php
+++ wp-content/themes/sebasTheme/functions.php
@@ -4,7 +4,42 @@ function meine_styles() {
     $handle = "sebas-theme-style";
     wp_enqueue_style( $handle, get_stylesheet_uri());
 }
-
 add_action('wp_enqueue_scripts', 'meine_styles');
 
 
+function add_my_widget() {
+
+   //
+
+}
+add_action( 'setup_theme', 'add_my_widget' );
+
+
+
+
+function add_my_menue() { 
+    register_nav_menus(
+        array(
+            'main-nav'   => 'Main Navigation',
+            'footer-nav' => 'Footer Navigation'
+        )
+    );
+}
+add_action( 'after_setup_theme', 'add_my_menue' );
+
+// Maybe an add_action can help:
+
+/*
+
+function my_custom_menus() {
+    $locations = array(
+        'fomenu'   => __( 'Fomenu', 'smaragdkerteszet' ),
+        'kismenu'  => __( 'Kismenu', 'smaragdkerteszet' ),
+    );
+    register_nav_menus( $locations );
+ }
+
+// Hook them into the theme-'init' action
+add_action( 'init', 'my_custom_menus' );
+
+*/
